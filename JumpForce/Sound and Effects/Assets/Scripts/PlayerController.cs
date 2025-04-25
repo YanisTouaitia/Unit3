9eb16bf747537754dc6d431ae1fcc9bbaf45ba84
Yanis Touaitia <touaitiay1@roverkids.org>
2025-04-25 14:16:43 -0400

    Added Jumping

--- JumpForce/Sound and Effects/Assets/Scripts/PlayerController.cs
+++ JumpForce/Sound and Effects/Assets/Scripts/PlayerController.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    public float jumpforce;
+    public float gravityModifier;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-        playerRb.AddForce(Vector3.up *500);
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Space))
+        {
+            playerRb.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
+        }
     }
 }
