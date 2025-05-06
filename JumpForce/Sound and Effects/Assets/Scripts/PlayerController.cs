fff1aa64c19a6ac7275f770ff402561449fafc25
Yanis Touaitia <touaitiay1@roverkids.org>
2025-05-06 13:05:42 -0400

    Adds game over and audio feedback
    Implements game over screen functionality and integrates audio feedback for jump and crash events.
    
    This provides the user with a visual and auditory cue when the game ends.

--- JumpForce/Sound and Effects/Assets/Scripts/PlayerController.cs
+++ JumpForce/Sound and Effects/Assets/Scripts/PlayerController.cs
@@ -8,12 +8,20 @@ public class PlayerController : MonoBehaviour
     public bool isOnGround = true;
     private Animator playerAnim;
     public bool gameOver = false;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    private AudioSource PlayerAudio;
+
+    
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
+        PlayerAudio = playerRb.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -24,21 +32,28 @@ public class PlayerController : MonoBehaviour
             playerRb.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
+            PlayerAudio.PlayOneShot(jumpSound, 1.0f);
         }
+
 
     }
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Ground"))
+        if (collision.gameObject.CompareTag("Ground") && !gameOver)
         {
             isOnGround = true;
+            dirtParticle.Play();
         }
-        else if (collision.gameObject.CompareTag("Obstacle"))
+        else if (collision.gameObject.CompareTag("Obstacle") && !gameOver)
         {
             gameOver = true;
             Debug.Log("Game Over!");
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
+            dirtParticle.Stop();
+            PlayerAudio.PlayOneShot(crashSound, 1.0f);
         }
     }
 }
